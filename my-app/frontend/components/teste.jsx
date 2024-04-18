0e21d09bef831e16bc7300cffe979c42bd76dcd2
Marco Antonio Rizzi Meneguetti <marco.meneguetti@sou.inteli.edu.br>
2024-04-17 22:12:48 -0300

    fixing things fodase

---
+++ my-app/frontend/components/teste.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Teste() {
+    return (
+        <div>
+            Hello, World!
+            <p style={{ color: 'black' }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit amet consequat aliquam, odio urna tincidunt nunc, id lacinia nisl lectus vitae nunc. Sed auctor, nunc id ultrices ultricies, lectus nunc tincidunt nunc, id lacinia nisl lectus vitae nunc.
+            </p>
+        </div>
+    );
+}
+
+export default Teste;
---
+++ my-app/frontend/pages/index.jsx
@@ -0,0 +1,9 @@
+import Teste from '../components/teste';
+
+export default function Home() {
+    return (
+        <div>
+        <Teste />
+        </div>
+    );
+    }
