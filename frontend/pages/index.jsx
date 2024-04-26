8c0acdc1af776e175fdf921490def725b54ddfb4
thigoulart <thiago.oliveira@sou.inteli.edu.br>
2024-04-25 21:55:21 -0300

    Footer responsivo

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,11 +1,13 @@
 import Contact from '../components/contact';
 import Rocket from '../components/rocket';
+import Footer from '../components/Footer';
 
 export default function Home() {
     return (
         <div>
-            {/* <Rocket /> */}
-            <Contact />
+            <Rocket />
+            {/* <Contact /> */}
+           {<Footer />}
         </div>
     );
     }
