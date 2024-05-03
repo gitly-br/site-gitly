c697836dfba7be772f5a00a1998aa492c051fae2
thigoulart <thiago.oliveira@sou.inteli.edu.br>
2024-05-02 21:20:35 -0300

    components

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -2,15 +2,21 @@ import Contact from '../components/contact';
 import Navbar from '@/components/navbar/navbar';
 import Action from '@/components/call-to-action/action';
 import Footer from '../components/footer';
+import Products from '../components/products';
+import Partners from '../components/partners';
+import Projects from '@/components/projects';
+
 
 export default function Home() {
     return (
         <div>
-            <Navbar></Navbar>
-            <Contact />
-            <Action />
+            {/* <Navbar></Navbar> */}
+            {/* <Contact /> */}
+            {/* <Action /> */}
+            <Products />
+            <Partners />
+            <Projects />
             <Footer />
-
         </div>
     );
     }
