6bba5ebe89a2831afc8bdc24df22856c110caa4a
luigiotavio <Luigi.Macedo@sou.inteli.edu.br>
2024-05-07 22:30:31 -0300

    Update - 1

--- frontend/components/ourclients.jsx
+++ frontend/components/ourclients.jsx
@@ -1,61 +1,31 @@
 import Image from "next/image";
-import Pessoa from '../assets/pessoa.jpeg'
-
+import Pessoa from '../assets/pessoa.png'
 
 function Ourclients() {
     return (
-        <div class = 'py-12 px-8 bg-[#001D09]'>
+        <div class = 'py-12 px-8 bg-black'>
             <div>
                 <h1 class = 'text-4xl text-white'>What our clients say</h1>
                 <p class = 'mt-3 text-white'>Lorem ipsum dolor sit amet consectetur adipiscing elit semper<br></br> dalar elementum tempus hac tellus libero accumsan. </p>
             </div>
 
             <div class = 'display:flex'>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-8"> 
-                    <div className="bg-[#092d19] p-4 rounded-lg flex flex-col md:flex-row items-center">
-                        <div className="md:w-1/2 md:pr-4">
-                            <Image src={Pessoa} alt="Imagem" className="rounded-lg"/>
+                <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-8"> 
+                    <div className="bg-[#001D09] rounded-lg flex flex-col sm:flex-row items-center justify-between">
+                        <div className="w-1/6 sm:w-1/2 pr-3">
+                            <Image src={Pessoa} alt="Imagem" className = 'w-full h-full'/>
                         </div>
-                        <div className="md:w-1/2 text-white md:pl-4">
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit ultrices scelerisque mi sed interdum lacus tellus in mi orc, netus nisl laoreet.</p>
-                            <p class = 'text-[#146237]'>
+                        <div className="w-5/6 text-white">
+                            <p className = 'text-base'>Lorem ipsum dolor sit amet consectetur adipiscing elit ultrices scelerisque mi sed interdum lacus tellus in mi orc, netus nisl laoreet.</p>
+                            <p class = 'text-[#146237] mt-5 text-base'>
                                 <b>Emily Blunt</b>
                             </p>
-                            <p>
-                                CEO at Krasinski
+                            <p className = 'mt-3 text-sm'>
+                                CEO at <u>Krasinski</u>
                             </p>
                         </div>
+          
                     </div>
-                    <div className="bg-[#092d19] p-4 rounded-lg flex flex-col md:flex-row items-center">
-                        <div className="md:w-1/2 md:pr-4">
-                            <Image src={Pessoa} alt="Imagem" width={600} height={450} className="rounded-lg"/>
-                        </div>
-                        <div className="md:w-1/2 text-white md:pl-4">
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit ultrices scelerisque mi sed interdum lacus tellus in mi orc, netus nisl laoreet.</p>
-                            <p class = 'text-[#146237]'>
-                                <b>Emily Blunt</b>
-                            </p>
-                            <p>
-                                CEO at Krasinski
-                            </p>
-                        </div>
-                    </div>
-                    <div className="bg-[#092d19] p-4 rounded-lg flex flex-col md:flex-row items-center">
-                        <div className="md:w-1/2 md:pr-4">
-                            <Image src={Pessoa} alt="Imagem" width={600} height={450} className="rounded-lg"/>
-                        </div>
-                        <div className="md:w-1/2 text-white md:pl-4">
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit ultrices scelerisque mi sed interdum lacus tellus in mi orc, netus nisl laoreet.</p>
-                            <p class = 'text-[#146237]'>
-                                <b>Emily Blunt</b>
-                            </p>
-                            <p>
-                                CEO at Krasinski
-                            </p>
-                        </div>
-                    </div>
-
-
                 </div>
             </div>
 
