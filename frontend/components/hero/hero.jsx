6dcc6efcd9f5ec93d80f791c5e8c29737628fdf2
luigiotavio <Luigi.Macedo@sou.inteli.edu.br>
2024-05-09 11:40:18 -0300

    feat: ajustes - 3

--- frontend/components/hero/hero.jsx
+++ frontend/components/hero/hero.jsx
@@ -8,7 +8,6 @@ function Hero() {
     return (
         <div class = "bg-black flex flex-col text-center justify-center items-center sm:text-center py-20">
             <h1 class = 'mt-5 font-title text-[6svw] md:text-[4svw]  text-white '>Let’s talk about AI-<br></br>Driven decision Making?</h1>
-
             <p class = 'font-text text-lg mt-5 text-white px-9 md:px-60 mb-10' >
             A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.
             </p>
