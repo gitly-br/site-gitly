6f1ea725165d9ac5618346d231c9206c7db6cdc0
Marco Antonio Rizzi Meneguetti <marco.meneguetti@sou.inteli.edu.br>
2024-05-09 11:51:59 -0300

    componente x finalizado

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -2,16 +2,18 @@ import Contact from '../components/contact';
 import Navbar from '@/components/navbar/navbar';
 import Action from '@/components/call-to-action/action';
 import Footer from '../components/footer';
+import ComponenteX from '../components/componente-X';
 
 export default function Home() {
     return (
        <div className='bg-[#000015] lg:bg-[#000015]'>
-            <Navbar></Navbar>
+            {/* <Navbar></Navbar>
             <div className='pt-40 px-5 xl:px-0 pb-36 xl:pb-36 xl:pt-28 bg-[#000015]'>
                 <Contact />
             </div> 
             <Action />
-            <Footer /> 
+            <Footer />  */}
+            <ComponenteX />
        </div>
     );
 }
