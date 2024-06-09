76faa4e39bbe938428b6b1695614056e49463515
luigiotavio <Luigi.Macedo@sou.inteli.edu.br>
2024-06-09 13:40:37 -0300

    feat(FRONT): Desenvolvimento componente talktoone

--- frontend/components/talktoone.jsx
+++ frontend/components/talktoone.jsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
-import Video from '../assets/video.svg';
 import React from "react";
 import ButtonGreenGet from "./button/buttonGreenGet";
-import Background from '../assets/background.svg';
 
 const talktoone = () => {
   return (
@@ -15,7 +12,10 @@ const talktoone = () => {
       </div>
 
       <div className="sm:col-span-2 w-full flex justify-center sm:block">
-        <Image src={Video} alt="Video" className="w-full h-full" />
+        <video autoPlay loop muted className="w-full h-full">
+          <source src="https://s3-figma-videos-production-sig.figma.com/video/1116531192233797349/TEAM/d681/f700/-c0b8-4f2a-b956-829b55b2e520?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=llxbZbNiVx05pMFFrEhioNYlhx2j4BEjwqA75zCQrbanvhfaJyNkbKP0hhJzc9ryP68urE74UzX3Rp-RNnKw4gmvTWhZYyztJOsPtm9P0HjgIMf2-rJPNsF7Vh8TIIPX4cMIM5aaeNW2GE8sARzp9~7aS0OxJCXo7SsQgIhGFMq4l1Ao~ioLXvOXUYtdMLGGMcDkRHQxPyjR2wBvNi7d4W8jbVl~S5BB5m4d7IYBG3YfAZJxM3WKr-kdTCgcUyHtsvVMhlXCG1vh84Oztyg8-Qb3vergUKSzN6doK-qQuT-x9gl2SbSuPM8o8dPWY1qBqP85Ob1TJg-3jZyzjFwygA__" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </div>
   );
--- frontend/pages/partners.jsx
+++ frontend/pages/partners.jsx
@@ -3,7 +3,6 @@ import Navbar from '@/components/navbar/navbar';
 import Action from '@/components/call-to-action/action';
 import Footer from '../components/footer';
 import Products from '../components/products';
-import Partners from '../components/partners';
 import Projects from '@/components/projects';
 
 import Hero from '@/components/hero/hero';
