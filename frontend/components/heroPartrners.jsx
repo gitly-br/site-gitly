a940a60e470f743f7bf061a8db11686b2142bb3e
miborim <mirella.lima@sou.inteli.edu.br>
2024-06-17 21:28:00 -0300

    feature: edita espaçamentos

--- frontend/components/heroPartrners.jsx
+++ frontend/components/heroPartrners.jsx
@@ -5,12 +5,12 @@ import original from "../assets/original.svg";
 
 const HeroPartners = () => {
     return (
-        <div className = "bg-black flex flex-col text-center justify-center items-center sm:text-center py-20">
-            <p className = 'uppercase font-text text-lg mt-5 text-white px-9 md:px-60 mb-10'>
+        <div className = "bg-black flex flex-col text-center justify-center items-center sm:text-center py-40">
+            <p className = 'uppercase font-text md:font-semibold text-white px-5 md:px-60'>
             Discover Our Proud Creations
             </p>
-            <h1 className = 'mt-5 font-title text-[6svw] md:text-[4svw]  text-white '>Take a Glimpse into Our<br></br>World</h1>
-            <div className="flex flex-col md:flex-row md:justify-center md:space-x-10 items-center w-3/5 sm:w-2/5 pr-3">
+            <h1 className = 'font-title text-[6svw] md:text-[3svw] text-white mb-10'>Take a Glimpse into Our<br></br>World</h1>
+            <div className="flex flex-col md:flex-row md:justify-center md:space-x-10 items-center w-3/5 sm:w-2/5 pr-3 space-y-5 md:space-y-0">
                 <Image src={iphone} alt="Imagem" className = 'w-full object-cover h-full'/>
                 <Image src={original} alt="Imagem" className = 'w-full object-cover h-full'/>
             </div>
