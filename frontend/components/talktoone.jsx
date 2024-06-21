7d42efacd960fd377912c5ef95e00ee073f46d67
luigiotavio <Luigi.Macedo@sou.inteli.edu.br>
2024-06-21 15:22:57 -0300

    feat(FRONT): Ajuste componente

--- frontend/components/talktoone.jsx
+++ frontend/components/talktoone.jsx
@@ -13,7 +13,7 @@ const talktoone = () => {
 
       <div className="sm:col-span-2 w-full flex justify-center sm:block">
         <video autoPlay loop muted className="w-full h-full">
-          <source src="https://s3-figma-videos-production-sig.figma.com/video/1116531192233797349/TEAM/d681/f700/-c0b8-4f2a-b956-829b55b2e520?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=llxbZbNiVx05pMFFrEhioNYlhx2j4BEjwqA75zCQrbanvhfaJyNkbKP0hhJzc9ryP68urE74UzX3Rp-RNnKw4gmvTWhZYyztJOsPtm9P0HjgIMf2-rJPNsF7Vh8TIIPX4cMIM5aaeNW2GE8sARzp9~7aS0OxJCXo7SsQgIhGFMq4l1Ao~ioLXvOXUYtdMLGGMcDkRHQxPyjR2wBvNi7d4W8jbVl~S5BB5m4d7IYBG3YfAZJxM3WKr-kdTCgcUyHtsvVMhlXCG1vh84Oztyg8-Qb3vergUKSzN6doK-qQuT-x9gl2SbSuPM8o8dPWY1qBqP85Ob1TJg-3jZyzjFwygA__" type="video/mp4" />
+          <source src="https://s3-figma-videos-production-sig.figma.com/video/1116531192233797349/TEAM/d681/f700/-c0b8-4f2a-b956-829b55b2e520?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=c2AuKIZE2djV5N991I6TgdgzDWci6S9osOoCRP8pmYnyg16fMWlhnnYbXLn-eq13umjQsemcdFNfb7YG~5pbqdGOb2RMsSv2FlYCcaH7YbySp9NQ5-21t0Bqt5UXJInri-UNJjrXOp3iigux2A3vxMnI9FRRgiI5-U3vrgNqwjkaJI4kz9h5WB6PSHuY7khvXW~e7XLF3jysZqgawga~3RWlEBRcNHB09Tg0yKBbOfYjYWn56lv2KGp5FX9N5PmutZmrTSRyRINQLhica7DuIOhMkKuDUwtp22Q4Wo8AYh3atPGHk98pipngugAkIFxqnG0ToOZ32ch-a55M7N-k7A__" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
