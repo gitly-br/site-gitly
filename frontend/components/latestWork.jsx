58df5930e8b610dc0e0f4249547443a8e0690065
thigoulart <thiago.oliveira@sou.inteli.edu.br>
2024-06-28 01:57:34 -0300

    ajustando componente nossos trabalhos

--- frontend/components/latestWork.jsx
+++ frontend/components/latestWork.jsx
@@ -1,41 +1,42 @@
 import React from 'react';
 import Image from "next/image";
-import container1 from "../assets/container1.svg";
-import container2 from "../assets/container2.svg";
-import container3 from "../assets/container3.svg";
+
+import latest1 from "../assets/latest1.svg";
+import latest2 from "../assets/latest2.svg";
+import latest3 from "../assets/latest3.svg";
 
 const LatestWork = () => {
   return (
     <div className="py-10 px-32 text-center flex flex-col bg-gradient-to-r from-[#021b09] to-black text-white paizao">
         <div>
-            <h1 className='text-[3em] font-semibold mb-5'>Our lasted work</h1>
-            <p className='text-lg md:text-xl lg:text-2xl text-center mb-5'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.</p>
+            <h1 className='text-[3em] font-semibold mb-5'>Nossos últimos trabalhos</h1>
+            <p className='text-lg md:text-xl lg:text-2xl text-center mb-5'>Descubra como as soluções inovadoras da Gitly estão transformando o cenário empresarial. Confira alguns dos nossos projetos mais recentes:</p>
         </div>
         <div className='flex gap-10 justify-center flex-wrap md:flex-nowrap pai'>
         <div className='relative flex-none'>
-            <Image className='w-full h-full object-cover' src={container3} alt="container3" />
+            <Image className='w-full h-full object-cover' src={latest1} alt="latest1"/>
             <div className='absolute inset-0 flex flex-col items-center justify-center text-center text-white mx-36 textoimage'>
-                <h1 className='text-lg font-bold mb-2'>White Label Mall Application</h1>
-                <p className='text-sm'>Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalaracc lacus vel facilisis.</p>
+                <h1 className='text-lg font-bold mb-2'>Solução de Dados para Varejo</h1>
+                <p className='text-sm'>Este sistema customizável permite que shoppings ofereçam uma experiência única de compra para seus clientes, suportando múltiplos usuários e facilitando a gestão de lojas.</p>
             </div>
         </div>
             <div className='flex flex-col gap-7'>
                 <div className='flex filho'>
-                    <div className='bg-[#202020] rounded-xl flex flex-col text-wrap text-start px-10 py-20'>
-                        <h1 className='font-semibold text-xl mb-3'>Multiple users</h1>
-                        <p className='text-[#9795B5]'>Lorem ipsum dolor sit amet consecte tur adipiscing elit semper.</p>
+                    <div className='bg-[#202020] rounded-xl flex flex-col text-wrap text-start px-5 py-10 w-[400px]'>
+                        <h1 className='font-semibold text-xl mb-3'>ChatBot baseado em IA</h1>
+                        <p className='text-[#9795B5]'>Nosso ChatBot inteligente utiliza aprendizado de máquina para fornecer atendimento ao cliente em tempo real. Capaz de lidar com várias consultas simultaneamente, a ferramenta aprende com as interações para melhorar continuamente sua eficácia.</p>
                     </div>
                     <div className='flex justify-center image'>
-                        <Image src={container2} alt="container2" />
+                        <Image src={latest2} alt="latest2" />
                     </div>
                 </div>
                 <div className='flex filho'>
-                    <div className='bg-[#202020] rounded-xl flex flex-col text-wrap text-start px-10 py-20'>
-                        <h1 className='font-semibold text-xl mb-3'>Multiple users</h1>
-                        <p className='text-[#9795B5]'>Lorem ipsum dolor sit amet consecte tur adipiscing elit semper.</p>
+                    <div className='bg-[#202020] rounded-xl flex flex-col text-wrap text-start px-5 py-10 w-[400px]'>
+                        <h1 className='font-semibold text-xl mb-3'>Serviços Customizados de IA</h1>
+                        <p className='text-[#9795B5]'>Na Gitly, criamos soluções de IA que se ajustam às necessidades específicas de cada cliente, melhorando a eficiência operacional e a experiência do usuário através de sistemas de gestão inovadores.</p>
                     </div>
                     <div className='flex justify-center image'>
-                        <Image src={container1} alt="container1" />
+                        <Image src={latest3} alt="latest3"/>
                     </div>
                 </div>
             </div>
