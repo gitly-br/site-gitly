fe23b538f01cebb0bce75ab60c036348bcd965cd
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2024-07-02 13:57:17 -0300

    fixed padding top page contact us

--- frontend/pages/contactus.jsx
+++ frontend/pages/contactus.jsx
@@ -1,20 +1,33 @@
 import Contact from '../components/contact';
 import Navbar from '@/components/navbar';
-// import Action from '@/components/action';
 import Footer from '../components/footer';
 import CallContact from '@/components/callContact';
-
 
 export default function Contactus() {
     return (
         <div>
-            <Navbar></Navbar>
-            <div className='pt-40 px-5 xl:px-0 pb-36 xl:pb-36 xl:pt-28 bg-[#000015]'>
+            <Navbar />
+            <div className='px-5 xl:px-0 pb-36 xl:pb-36 top' style={{ background: 'linear-gradient(to bottom, #00002D, #000015)' }}>
                 <Contact />
             </div>
-            {/* <Action /> */}
             <CallContact />
             <Footer />
+            <style jsx>{`
+                .top {
+                    padding-top: 290px;
+                }
+                
+                @media (min-width: 1800px) {
+                    .top {
+                        padding-top: 100px;
+                }
+                
+                @media (max-width: 1238px) {
+                    .top {
+                        padding-top: 100px;
+                    }
+                }
+            `}</style>
         </div>
     );
 }
