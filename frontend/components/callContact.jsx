a32d9fcda83a116ebe9aad1d7e52ec3e4d9d9cbc
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2024-07-02 17:09:18 -0300

    refactor CallAction

--- frontend/components/callContact.jsx
+++ frontend/components/callContact.jsx
@@ -1,26 +1,58 @@
-import Image from 'next/image'
-import computador from '../assets/computador.svg'
+import Image from 'next/image';
+import Computer from '../assets/computer.svg';
+import Bgleft from '../assets/bg-left.svg';
+import Ballright from '../assets/ball-center.svg';
+
 
 function CallContact() {
     return (
-        <div className="relative flex items-center justify-between h-screen bg-gradient-to-r from-[#021b09] to-black">
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <div className="relative z-10 flex flex-col md:flex-row-reverse items-center justify-between w-full px-8 md:px-16 lg:px-32">
-                <div className="w-full md:w-1/2 flex flex-col items-start text-white space-y-6">
-                    <h1 className="text-3xl md:text-4xl font-title">Fale com um de nossos especialistas e inicie seu projeto hoje!</h1>
-                    <p className="text-base md:text-lg font-text">
-                    Na Gitly, estamos prontos para ajudar sua empresa a transformar dados em decisões inteligentes. Se você deseja mais informações sobre nossos serviços, precisa de ajuda para começar um projeto ou simplesmente quer explorar possibilidades de parceria, entre em contato conosco. Nossa equipe de especialistas está mais do que feliz em atendê-lo e fornecer todas as informações que você precisa.
-                    </p>
-                    <button className="bg-green rounded-br-lg w-48 h-12 text-white font-text">
-                    Inicie seu Projeto
-                    </button>
+        <div className="relative flex flex-col lg:flex-row items-center justify-center h-screen gap-20 px-5 xl:px-20 py-20 bg-black beckgraund">
+            {/* Background SVGs */}
+            <div className="absolute top-0 left-0 hidden-on-small2">
+                <Image src={Bgleft} alt="Background Left" layout="fixed" width={620} height={700} />
+            </div>
+            <div className="absolute top-0 right-0 hidden-on-small">
+                <Image src={Ballright} alt="Background Top Right" layout="fixed" width={1000} height={1200} />
+            </div>
+            {/* Content */}
+            <div className="flex flex-col items-center lg:items-start text-center lg:text-left z-10 text-white response">
+                <div>
+                    <h1 className="text-3xl font-semibold">Talk to one of our specialists and start your project today</h1>
                 </div>
-                <div className="w-full md:w-1/2 flex justify-center md:justify-end mb-8 md:mb-0">
-                    <Image src={computador} alt="Computador" width={500} height={500} className="object-contain" />
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit dolor semper at ac tempus enim laoreet massa non.</p>
+                </div>
+                <div>
+                    <button className="bg-[#1DB954] text-white py-2 px-4 rounded mt-4">Get a free quote</button>
                 </div>
             </div>
+            <div className="flex flex-col items-center justify-center z-10">
+                <Image src={Computer} alt="Computer" width={759.29} height={889.84} />
+            </div>
+            <style jsx>{`
+            @media (max-width: 1560px) {
+            .hidden-on-small {
+                display: none;
+            }
+            
+            @media (max-width: 1024px) {
+            .hidden-on-small2 {
+                display: none;
+            }
+            
+            .beckgraund {
+                background: linear-gradient(to right, #021b09, black);
+            }
+
+            .response {
+                padding-top: 100px;
+            }
+
+            `}
+
+            </style>
         </div>
-    )
+    );
 }
 
-export default CallContact
+export default CallContact;
--- frontend/pages/contactus.jsx
+++ frontend/pages/contactus.jsx
@@ -10,21 +10,33 @@ export default function Contactus() {
             <div className='px-5 xl:px-0 pb-36 xl:pb-36 top' style={{ background: 'linear-gradient(to bottom, #00002D, #000015)' }}>
                 <Contact />
             </div>
-            <CallContact />
+            <div className='pb-24 back'><CallContact /></div>
             <Footer />
             <style jsx>{`
                 .top {
                     padding-top: 290px;
                 }
-                
-                @media (min-width: 1800px) {
-                    .top {
-                        padding-top: 100px;
-                }
-                
+
                 @media (max-width: 1238px) {
                     .top {
                         padding-top: 100px;
+                    }
+                }
+
+                @media (max-width: 1024px) {
+                    .back {
+                        background: linear-gradient(to right, #021b09, black);
+                    }
+                }
+
+                @media (min-width: 1472px) {
+                    .top {
+                        padding-top: 100px;
+                }
+
+                @media (min-width: 1025px) {
+                    .back {
+                        background: black;
                     }
                 }
             `}</style>
